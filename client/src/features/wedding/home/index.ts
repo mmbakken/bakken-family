c2ff6a321d8f86bea9941e8ea6f35dd6976f259f
Matthew Bakken <mmbakken@gmail.com>
2025-07-20 09:37:48 -0600

    Add FAQs page content.

--- client/src/features/wedding/home/index.ts
+++ client/src/features/wedding/home/index.ts
@@ -1,4 +1,5 @@
 export { default as Events } from './Events'
+export { default as Faq } from './Faq'
 export { default as Header } from './Header'
 export { default as Home } from './Home'
 export { default as OurStory } from './OurStory'

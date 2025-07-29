236fd28eab564bd595488510c1680b633e541192
Matthew Bakken <mmbakken@gmail.com>
2025-07-28 23:56:58 -0600

    Add Things to Do page content.

--- client/src/features/wedding/home/Questions.tsx
+++ client/src/features/wedding/home/Questions.tsx
@@ -275,7 +275,7 @@ const Questions = () => {
         />
       </main>
 
-      <footer>
+      <footer className="pb-2">
         <Link to="/wedding">
           <Button variant="link">Home</Button>
         </Link>
